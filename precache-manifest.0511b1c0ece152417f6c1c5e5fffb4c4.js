197588f16806877f82521ac92430136010f8f57f
Ramya <ramyagatti361@gmail.com>
2020-08-28 13:52:59 +0530

    Updates

---
+++ precache-manifest.0511b1c0ece152417f6c1c5e5fffb4c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2fdcb2009b08f4492a61cca1ea301b",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "3bf0ae426a781aff0ee6",
+    "url": "/covid-tracker/static/css/main.5c3b3087.chunk.css"
+  },
+  {
+    "revision": "2f172c1e18538d5ab359",
+    "url": "/covid-tracker/static/js/2.1faba153.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-tracker/static/js/2.1faba153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf0ae426a781aff0ee6",
+    "url": "/covid-tracker/static/js/main.5066e610.chunk.js"
+  },
+  {
+    "revision": "dd7bbf5f98d9d5f1ef3c",
+    "url": "/covid-tracker/static/js/runtime-main.78f2ce21.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
